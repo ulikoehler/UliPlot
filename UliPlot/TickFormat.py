0022416707f4578ee4e3dc56fa4318181810e54b
Uli Köhler <ukoehler@techoverflow.net>
2021-03-05 23:53:00 +0100

    Added date tick format utilities

--- UliPlot/TickFormat.py
+++ UliPlot/TickFormat.py
@@ -1,12 +1,13 @@
 #!/usr/bin/env python3
 from matplotlib import pyplot as plt
 import matplotlib.ticker as mtick
+import matplotlib.dates as mdates
 
-def set_axis_tickformat_func(func, axis, set_major=True, set_minor=True):
+def set_axis_tick_format_func(func, axis, set_major=True, set_minor=True):
     """
-    Set the tick formatter for an X or Y axis to set_axis_tickformat_func
+    Set the tick formatter for an X or Y axis to set_axis_tick_format_func
     Typically, this is not called directly.
-    Usually you should use set_xaxis_tickformat_func() or set_yaxis_tickformat_func()
+    Usually you should use set_xaxis_tick_format_func() or set_yaxis_tick_format_func()
     instead.
     """
     formatter = mtick.FuncFormatter(func)
@@ -15,7 +16,7 @@ def set_axis_tickformat_func(func, axis, set_major=True, set_minor=True):
     if set_minor:
         axis.yaxis.set_minor_formatter(formatter)
 
-def set_yaxis_tickformat_func(func, ax=None, set_major=True, set_minor=True):
+def set_yaxis_tick_format_func(func, ax=None, set_major=True, set_minor=True):
     """
     For the current Y axis, set <func(value, index)> as the (major & minor) tick formatter.
     This is often used with lambda functions.
@@ -29,10 +30,10 @@ def set_yaxis_tickformat_func(func, ax=None, set_major=True, set_minor=True):
     """
     if ax is None:
         ax = plt.gca()
-    set_axis_tickformat_func(func, ax.yaxis, set_major=set_major, set_minor=set_minor)
+    set_axis_tick_format_func(func, ax.yaxis, set_major=set_major, set_minor=set_minor)
 
 
-def set_xaxis_tickformat_func(func, ax=None, set_major=True, set_minor=True):
+def set_xaxis_tick_format_func(func, ax=None, set_major=True, set_minor=True):
     """
     For the current Y axis, set <func(value, index)> as the (major & minor) tick formatter.
     This is often used with lambda functions.
@@ -46,13 +47,13 @@ def set_xaxis_tickformat_func(func, ax=None, set_major=True, set_minor=True):
     """
     if ax is None:
         ax = plt.gca()
-    set_axis_tickformat_func(func, ax.xaxis, set_major=set_major, set_minor=set_minor)
+    set_axis_tick_format_func(func, ax.xaxis, set_major=set_major, set_minor=set_minor)
 
-def set_axis_tick_format(axis, decimals=3, unit=None, set_major=True, set_minor=True):
+def set_axis_tick_format_float(axis, decimals=3, unit=None, set_major=True, set_minor=True):
     """
     Set the number of decimals and the unit for formatting the given X or Y axis.
     Typically, this is not called directly.
-    Usually you should use set_xaxis_tick_format() or set_yaxis_tick_format()
+    Usually you should use set_xaxis_tick_format_float() or set_yaxis_tick_format_float()
     instead.
     """
     # Create format string
@@ -66,7 +67,7 @@ def set_axis_tick_format(axis, decimals=3, unit=None, set_major=True, set_minor=
     if set_minor:
         axis.set_minor_formatter(formatter)
 
-def set_yaxis_tick_format(decimals=3, unit=None, ax=None, set_major=True, set_minor=True):
+def set_yaxis_tick_format_float(decimals=3, unit=None, ax=None, set_major=True, set_minor=True):
     """
     Set the number of decimals and the unit for formatting the Y axis.
     """
@@ -74,9 +75,9 @@ def set_yaxis_tick_format(decimals=3, unit=None, ax=None, set_major=True, set_mi
     if ax is None:
         ax = plt.gca()
     # Set formatter
-    set_axis_tick_format(ax.yaxis, decimals=decimals, unit=unit, set_major=set_major, set_minor=set_minor)
+    set_axis_tick_format_float(ax.yaxis, decimals=decimals, unit=unit, set_major=set_major, set_minor=set_minor)
 
-def set_xaxis_tick_format(decimals=3, unit=None, ax=None, set_major=True, set_minor=True):
+def set_xaxis_tick_format_float(decimals=3, unit=None, ax=None, set_major=True, set_minor=True):
     """
     Set the number of decimals and the unit for formatting the Y axis.
     """
@@ -84,4 +85,58 @@ def set_xaxis_tick_format(decimals=3, unit=None, ax=None, set_major=True, set_mi
     if ax is None:
         ax = plt.gca()
     # Set formatter
-    set_axis_tick_format(ax.xaxis, decimals=decimals, unit=unit, set_major=set_major, set_minor=set_minor)
+    set_axis_tick_format_float(ax.xaxis, decimals=decimals, unit=unit, set_major=set_major, set_minor=set_minor)
+
+def set_axis_tick_format_date(axis, dateformat="%Y%m%d %H%M%S", tz=None, set_major=True, set_minor=True):
+    """
+    Set the date format for formatting the given X or Y axis.
+    Typically, this is not called directly.
+    Usually you should use set_xaxis_tick_format_date() or set_yaxis_tick_format_date()
+    instead.
+
+    :param axis: The axis (X or Y) to set the formatter for
+    :param str dateformat: The strftime date format string, e.g. "%Y%m%d %H%M%S"
+    :param datetime.tzinfo tz: The time zone for the ticks, uses matplotlib default if None
+    :param bool set_major: If True, set the formatter as major tick formatter
+    :param bool set_minor: If True, set the formatter as minor tick formatter
+    """
+    # Set formatter
+    formatter = mdates.DateFormatter(dateformat, tz=tz)
+    if set_major:
+        axis.set_major_formatter(formatter)
+    if set_minor:
+        axis.set_minor_formatter(formatter)
+
+
+def set_yaxis_tick_format_date(dateformat="%Y%m%d %H%M%S", tz=None, ax=None, set_major=True, set_minor=True):
+    """
+    Set the number of decimals and the unit for formatting the Y axis.
+    
+    :param str dateformat: The strftime date format string, e.g. "%Y%m%d %H%M%S"
+    :param datetime.tzinfo tz: The time zone for the ticks, uses matplotlib default if None
+    :param Axis ax: You can specify an ax object here. If None, plt.gca() is used.
+    :param bool set_major: If True, set the formatter as major tick formatter
+    :param bool set_minor: If True, set the formatter as minor tick formatter
+    """
+    # Create ax unless specfied
+    if ax is None:
+        ax = plt.gca()
+    # Set formatter
+    set_axis_tick_format_date(ax.yaxis, dateformat=dateformat, tz=tz, set_major=set_major, set_minor=set_minor)
+
+def set_xaxis_tick_format_date(dateformat="%Y%m%d %H%M%S", tz=None, ax=None, set_major=True, set_minor=True):
+    """
+    Set the number of decimals and the unit for formatting the X axis.
+    
+    :param str dateformat: The strftime date format string, e.g. "%Y%m%d %H%M%S"
+    :param datetime.tzinfo tz: The time zone for the ticks, uses matplotlib default if None
+    :param Axis ax: You can specify an ax object here. If None, plt.gca() is used.
+    :param bool set_major: If True, set the formatter as major tick formatter
+    :param bool set_minor: If True, set the formatter as minor tick formatter
+    """
+    # Create ax unless specfied
+    if ax is None:
+        ax = plt.gca()
+    # Set formatter
+    set_axis_tick_format_date(ax.xaxis, dateformat=dateformat, tz=tz, set_major=set_major, set_minor=set_minor)
+
