0f0348b867dc5e7a09c735670a143fae978db248
Uli Köhler <ukoehler@techoverflow.net>
2021-09-23 04:44:41 +0200

    Fix auto XLSX width algorithm with index=False

--- UliPlot/XLSX.py
+++ UliPlot/XLSX.py
@@ -17,16 +17,17 @@ def auto_adjust_xlsx_column_width(df, writer, sheet_name, margin=2, index=True):
     :param int margin: How many extra space (beyond the maximum size of the string)
     :param bool index: Whether the DataFrame's index is inserted as a separate column (if index=False in df.to_xlsx() set index=False here!)
     """
-    for column in df.columns:
+    for column_name in df.columns:
         # Convert the value of the columns to string and select the 
-        column_length =  max(df[column].astype(str).map(len).max(), len(column))
+        column_length =  max(df[column_name].astype(str).map(len).max(), len(column_name))
         # Get index of column in XLSX
         # Column index is +1 if we also export the index column
-        col_idx = df.columns.get_loc(column)
+        col_idx = df.columns.get_loc(column_name)
         if index:
             col_idx += 1
         # Set width of column to (column_length + margin)
         writer.sheets[sheet_name].set_column(col_idx, col_idx, column_length + margin)
-    # Compute column width with index
-    index_length =  max(df.index.astype(str).map(len).max(), len(column))
-    writer.sheets[sheet_name].set_column(0, 0, index_length + margin)
+    # Compute column width of index column (if enabled)
+    if index: # If the index column is being exported
+        index_length =  max(df.index.astype(str).map(len).max(), len(column))
+        writer.sheets[sheet_name].set_column(0, 0, index_length + margin)
